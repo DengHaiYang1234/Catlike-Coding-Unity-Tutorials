e9a17c487bb22d42a3880e5cfed622bb07f7390d
Jasper Flick <support@catlikecoding.com>
2019-04-25 12:28:31 +0200

    Initial commit.

---
+++ Assets/Scripts/GameTileContent.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class GameTileContent : MonoBehaviour {
+
+	[SerializeField]
+	GameTileContentType type = default;
+
+	GameTileContentFactory originFactory;
+
+	public GameTileContentType Type => type;
+
+	public GameTileContentFactory OriginFactory {
+		get => originFactory;
+		set {
+			Debug.Assert(originFactory == null, "Redefined origin factory!");
+			originFactory = value;
+		}
+	}
+
+	public void Recycle () {
+		originFactory.Reclaim(this);
+	}
+}
